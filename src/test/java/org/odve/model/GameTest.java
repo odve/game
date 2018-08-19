688ce26dffb70cc9f51e952c5dcf359faea2e7ed
Evgenii <vomikodve@gmail.com>
2018-08-20 02:22:51 +0300

    add assertJ

---
+++ src/test/java/org/odve/model/GameTest.java
@@ -0,0 +1,17 @@
+package org.odve.model;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author evdokimov evgenii
+ */
+
+public class GameTest {
+
+    @Test
+    public void createGame() {
+        assertThat(true).isTrue();
+    }
+}
