7c6d1d1bd9ddbb96706877732a315c739c9e31e4
Evgenii <vomikodve@gmail.com>
2018-08-23 00:37:14 +0300

    add logger

--- src/main/java/org/odve/lang/GameRulesParser.java
+++ src/main/java/org/odve/lang/GameRulesParser.java
@@ -1,5 +1,6 @@
 package org.odve.lang;
 
+import lombok.extern.slf4j.Slf4j;
 import org.odve.model.GameRules;
 
 import java.io.InputStream;
@@ -8,12 +9,16 @@ import java.io.InputStream;
  * @author evdokimov evgenii
  */
 
+@Slf4j
 public class GameRulesParser {
 
     public GameRules parse(InputStream rules) {
+        log.debug("rules parsing started");
+
         GameRules.GameRulesBuilder builder = GameRules.builder();
         builder.name("chess");
 
+        log.debug("rules parsing finished");
         return builder.build();
     }
 }
