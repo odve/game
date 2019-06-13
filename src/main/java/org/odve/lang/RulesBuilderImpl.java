c83e9f225c8a0799e8437b56b30e5d5319594005
Evgenii <vomikodve@gmail.com>
2019-06-14 01:30:25 +0300

    Add simple chess board rule

--- src/main/java/org/odve/lang/RulesBuilderImpl.java
+++ src/main/java/org/odve/lang/RulesBuilderImpl.java
@@ -1,5 +1,7 @@
 package org.odve.lang;
 
+import org.odve.lang.libraries.board.ChessCell;
+import org.odve.lang.libraries.board.SquareBoard;
 import org.odve.lang.libraries.order.Order;
 import org.odve.model.GameRules;
 import org.odve.model.Player;
@@ -13,6 +15,7 @@ import java.util.Map;
  * @author evdokimov evgenii
  */
 
+//TODO: conside moving load class logic here
 public class RulesBuilderImpl implements RulesBuilder {
 
     private final GameRules.GameRulesBuilder builder;
@@ -49,5 +52,12 @@ public class RulesBuilderImpl implements RulesBuilder {
     public void order(String orderName, List<String> params) {
         Order order = Order.createOrder(orderName, params, playerMap);
         builder.order(order);
+    }
+
+    @Override
+    public void board(String boardType, List<String> params) {
+        int x = Integer.valueOf(params.get(0));
+        int y = Integer.valueOf(params.get(1));
+        builder.board(new SquareBoard(x, y, new ChessCell.Factory()));
     }
 }
---
+++ src/main/java/org/odve/lang/libraries/board/SquareBoard.java
@@ -0,0 +1,38 @@
+package org.odve.lang.libraries.board;
+
+import com.sun.tools.javac.util.Pair;
+
+/**
+ * @author evdokimov evgenii
+ */
+
+public class SquareBoard {
+
+    private final int x;
+    private final int y;
+    private final Cell[][] cells;
+
+    public SquareBoard(int x, int y, CellFactory cellFactory) {
+        this.x = x;
+        this.y = y;
+
+        cells = new Cell[x][y];
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                cells[i][j] = cellFactory.createCell(i, j);
+            }
+        }
+    }
+
+    public Pair<Integer, Integer> getSize() {
+        return Pair.of(x, y);
+    }
+
+    public Cell getCell(int x, int y) {
+        if (x < 0 || x >= this.x || y < 0 || y >= this.y) {
+            throw new IllegalArgumentException("incorrect x,y pare: " + x + ", " + y);
+        }
+
+        return cells[x][y];
+    }
+}
